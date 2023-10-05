49e1b7fe1cca3bc9d9cbcd0f2617540e5d69ed52
Matias <matiasandinoma@gmail.com>
2023-10-05 11:33:55 -0300

    useFetch, MultipleCustomHooks getApi utilizando un hook useCounter

---
+++ src/components/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import '../02-useEffect/effect.css';
+import { useFetch } from '../../hooks/useFetch';
+import { useCounter } from '../../hooks/useCounter2';
+
+export const MultipleCustomHooks = () => {
+    
+    const { counter, increment } = useCounter( 1 );
+
+    const { loading, data } = useFetch( `https://pokeapi.co/api/v2/pokemon/${ counter }` );
+
+    const { name, order } = !!data && data;
+
+    return (
+        <div>
+            <h1>API</h1>
+            <hr />
+
+            {
+                loading
+                ?
+                    (
+                        <div className="alert alert-info text-center">
+                            Loading...
+                        </div>
+                    ) 
+                : 
+                    (
+                        <blockquote className='blockquote text-center'>
+                            <p className='mb-0'>{ name }</p>
+                            <hr />
+                            <footer className='blockquote-footer'>{ order }</footer>
+                        </blockquote>
+                    )
+            }
+
+            <button onClick={ increment } className='btn btn-primary'>NEXT</button>
+
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client'
 // import {CounterApp} from './components/01-useState/CounterApp'
 // import {CounterWithCustomHook} from './components/01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './components/02-useEffect/SimpleForm'
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <CounterApp /> */}
     {/* <CounterWithCustomHook /> */}
     {/* <SimpleForm /> */}
-    <FormWithCustomHook />
+    {/* <FormWithCustomHook /> */}
+    <MultipleCustomHooks />
   </React.StrictMode>,
 )
