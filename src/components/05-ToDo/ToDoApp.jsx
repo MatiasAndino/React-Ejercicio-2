a3f54a7314767f5dea09258cac1db401f7bb92b0
Matias <matiasandinoma@gmail.com>
2023-10-09 19:26:55 -0300

    ToDoApp completo, pero le falta dividir en componentes

--- src/components/05-ToDo/ToDoApp.jsx
+++ src/components/05-ToDo/ToDoApp.jsx
@@ -1,21 +1,68 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import { toDoListReducer } from "./toDoListReducer";
+import { useForm } from "../../hooks/useForm";
 
 
 import './ToDoApp.css';
 
-const initialState = [
-    {
-        id: new Date().getDate(),
-        item: 'Dale perro',
-        done: false,
-    }
-];
+const init = () => {
+
+    return JSON.parse(localStorage.getItem('ToDoList')) || [];
+    
+};
 
 export const ToDoApp = () => {
 
-    const [ toDoList ] = useReducer(toDoListReducer, initialState);
+    const [ toDoList, dispatch ] = useReducer(toDoListReducer, [], init);
 
+    const [ { description }, handleInputChange, reset ] = useForm({
+        description:''
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (description.trim().length <= 1) return;
+
+        const newToDoItem = {
+            id: new Date().getTime(),
+            item: description,
+            done: false,
+        };
+
+        const action = {
+            type: 'add',
+            payload: newToDoItem,
+        };
+
+        dispatch( action );
+        
+        reset();
+    }
+    
+    const handleDelete = ( toDoId ) => {
+    
+        const action = {
+            type: 'delete',
+            payload: toDoId,
+        };
+    
+        dispatch( action );
+
+    }
+
+    const handleToggle = (toDoId) => {
+        const action = {
+            type: 'toggle',
+            payload: toDoId,
+        }
+
+        dispatch( action);
+    }
+
+    useEffect(() => {
+        localStorage.setItem('ToDoList', JSON.stringify( toDoList ));
+    }, [toDoList]);
 
     return (
         <>
@@ -33,16 +80,20 @@ export const ToDoApp = () => {
                     toDoList.map( (toDo, i) => (
                         <li
                             key={ toDo.id }
-                            className="list-group-item"
+                            className="list-group"
                         >
                             <div
                                 className="item"
                             >
-                                <p>
+                                <p
+                                    className={`${ toDo.done && 'complete' }`}
+                                    onClick={ () => handleToggle( toDo.id ) }
+                                >
                                     { i + 1 }. { toDo.item }
                                 </p>
                                 <button
                                     className="btn btn-danger"
+                                    onClick={ () => handleDelete( toDo.id ) }
                                 >
                                     Borrar
                                 </button>
@@ -51,6 +102,27 @@ export const ToDoApp = () => {
                     ))
                 }
             </ul>
+            <div>
+                <form 
+                    onSubmit={handleSubmit}
+                >
+                    <input 
+                        type="text" 
+                        name="description" 
+                        className="form-control mt-5"
+                        placeholder="Agrega una nueva tarea"
+                        autoComplete="off"
+                        onChange={ handleInputChange }
+                        value={ description }
+                    />
+                    <button 
+                        type="submit"
+                        className="btn btn-outline-primary mt-3"
+                    >
+                        Agregar
+                    </button>
+                </form>
+            </div>
         </>
     )
 }
--- src/components/05-ToDo/toDoListReducer.js
+++ src/components/05-ToDo/toDoListReducer.js
@@ -1,10 +1,20 @@
 
 export const toDoListReducer = (state = [], action) => {
     switch (action.type) {
-        case add:
-            
-            break;
-    
+        case 'add':
+            return [...state, action.payload];
+        case 'delete':
+            return state.filter(item => item.id !==action.payload);
+        case 'toggle':
+            return state.map( item => {
+                if ( item.id === action.payload ) {
+                    return {
+                        ...item,
+                        done: !item.done,
+                    }
+                }
+                return item;
+            });
         default:
             return state;
     }
